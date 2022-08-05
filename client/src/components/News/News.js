361b21b724b40ef0d9484a13dff3943dc5a026cc
Adi Shuker <Adielgali57@gmail.com>
2022-08-05 12:00:15 +0300

    news and update users

--- client/src/components/News/News.js
+++ client/src/components/News/News.js
@@ -1,30 +1,25 @@
 import React, { useState } from "react";
-import { useSpring, animated } from "react-spring";
 import BackIcon from "../UsersComponents/Icons/BackIcon";
-import { useHistory } from "react-router-dom";
-const News = ({ text }) => {
-  text = "hellohellohellohello";
-  const [key, setKey] = useState(1);
-  const scrolling = useSpring({
-    from: { y: 0 },
-    to: { y: 400 },
-    config: { duration: 10000 },
-    reset: true,
-    onRest: () => {
-      setKey(key + 1);
-    },
-  });
-  const history = useHistory();
-  return (
+import {NewsContext} from "../../App";
+import PresentOneNews from "./PresentOneNews.js";
+import {useHistory} from "react-router-dom";
+import Header from "../UsersComponents/Header";
+
+const News = () => {
+    const { news, setNews } = React.useContext(NewsContext);
+    const history = useHistory();
+    return (
     <div className="newsDiv">
-      <div key={key}>
-        <animated.div style={scrolling}>{text}</animated.div>
-      </div>
-      <div
-        className="BackIconDiv iconWrapper lightGreyBorder"
-        onClick={() => history.push("/usersHomePage")}
-      >
-        <BackIcon />
+        <Header />
+        <h3>חדשות ועדכונים</h3>
+      <div className="BackIconDiv iconWrapper lightGreyBorder">
+          {news.map((item)=>{
+             return <PresentOneNews key={item.id} title={item.title} date={item.date} content={item.content} link={item.link} />
+          })}
+          <div onClick={() => history.push("/usersHomePage")}>
+              <BackIcon />
+          </div>
+
       </div>
     </div>
   );
